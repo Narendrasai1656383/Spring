2a5543790604dee3f43e69dd2e4b350e0868a192
next <next@next-9804>
2025-02-04 17:10:36 +0530

    Initial commit

---
+++ first-spring-framework/src/test/java/com/firstSpring/first_spring_framework/FirstSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstSpring.first_spring_framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
