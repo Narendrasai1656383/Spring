aaf67ea27a496d21af8ab2dc365989c858c26ac3
next <next@next-9804>
2025-02-04 17:29:47 +0530

    commit

---
+++ first-spring-framework/src/main/java/com/firstSpring/first_spring_framework/NormController.java
@@ -0,0 +1,13 @@
+package com.firstSpring.first_spring_framework;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NormController {
+   @RequestMapping("/")
+   public String method() {
+	   return "My first one";
+   }
+}
