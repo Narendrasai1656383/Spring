6690215929a5219dfc7521a2230cb57dee37b4e2
Narendrasai1656383 <narendrasai547@gmail.com>
2025-03-03 13:44:48 +0530

    commit

--- inversion_control/src/main/java/com/example/inversion_control/InversionControlApplication.java
+++ inversion_control/src/main/java/com/example/inversion_control/InversionControlApplication.java
@@ -1,13 +1,15 @@
 package com.example.inversion_control;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class InversionControlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InversionControlApplication.class, args);
+		//spring bean container
+		AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(MobilesConfig.class);
+		Mobiles obj=context.getBean("getOnePlusObject",Mobiles.class);
+		obj.getModelAndColor();
 	}
-
 }
